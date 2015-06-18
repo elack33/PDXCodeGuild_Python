9a0c9d6bdf230850617e1b3c11580bb11e288b6d
Dar Wright <elack33@gmail.com>
2015-06-17 21:22:21 -0700

    Commit for the end of the day.

--- stopwatch.py
+++ stopwatch.py
@@ -8,7 +8,7 @@ STARTTIME = time.time()
 class StopWatch:
     def __init__(self):
         global STARTTIME
-        ui()
+        self.ui()
 
     def starttime(self):
         global STARTTIME
@@ -20,35 +20,56 @@ class StopWatch:
         endtime = time.time()
         return endtime
 
-    def time_between(self, endtime, starttime):
+    def time_between(self, endtime, STARTTIME):
         # global STARTTIME, ENDTIME
-        time_between = endtime - starttime
+        time_between = endtime - STARTTIME
         return time_between()
 
-    def format_time(self,new_time):
-        st = datetime.datetime.fromtimestamp(new_time).strftime('%Y-%m-%d %H:%M:%S')
-        return st
+    def format_time(self, new_time):
+        formatted = datetime.datetime.fromtimestamp(new_time).strftime('%Y-%m-%d %H:%M:%S')
+        return formatted
 
     def ui(self):
         global STARTTIME
         while True:
             choice = raw_input( 'Welcome to the stop watch!\n'
                                 'Enter a command:\n'
-                                '~~~~~~~~~~~~~~~~'
+                                '~~~~~~~~~~~~~~~\n'
                                 '1 to START!\n'
                                 '2 to STOP\n'
-                                '3 to quit'
+                                '3 to quit\n'
                                 '>> ')
             if choice == 1:
-                STARTTIME = starttime()
+                STARTTIME = self.starttime()
+                self.ui()
             elif choice == 2:
-                stop = stoptime()
-                time_passed = time_bewtween(stop, STARTTIME)
-                format_timepassed = format_time(time_passed)
+                stop = self.stoptime()
+                time_passed = self.time_bewtween(stop, STARTTIME)
+                format_timepassed = self.format_time(time_passed)
                 print format_timepassed, "has passed."
+                raw_input("Press enter to continue...")
+                self.ui()
             else:
                 print "Thank you for using my stopwatch!"
                 exit()
 
 
 StopWatch()
+
+
+# Reinas basic example
+# class StopWatch(object):
+#
+#     def start(self):
+#         self.start = time.time()
+#
+#
+#     def stop(self):
+#         self.end = time.time()
+#         print '{} seconds'.format(self.end - self.start)
+
+
+
+# stopwatch = StopWatch()
+# stopwatch.start()
+# stopwatch.stop()
