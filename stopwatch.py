91301593e87f4d1eb97906316e79e11e13f34ac9
Dar Wright <elack33@gmail.com>
2015-06-18 18:13:31 -0700

    Fixed stop watch, narrowed time return to minutes and seconds, made it work
    still need to clean up the commented out code

--- stopwatch.py
+++ stopwatch.py
@@ -16,35 +16,35 @@ class StopWatch:
         return STARTTIME
 
     def stoptime(self):
-        # global ENDTIME
         endtime = time.time()
         return endtime
 
-    def time_between(self, endtime, STARTTIME):
-        # global STARTTIME, ENDTIME
+    def time_between(self, endtime):
         time_between = endtime - STARTTIME
-        return time_between()
+        return time_between
 
     def format_time(self, new_time):
-        formatted = datetime.datetime.fromtimestamp(new_time).strftime('%Y-%m-%d %H:%M:%S')
+        formatted = datetime.datetime.fromtimestamp(new_time).strftime('%M:%S')
         return formatted
 
     def ui(self):
         global STARTTIME
         while True:
-            choice = raw_input( 'Welcome to the stop watch!\n'
-                                'Enter a command:\n'
-                                '~~~~~~~~~~~~~~~\n'
-                                '1 to START!\n'
-                                '2 to STOP\n'
-                                '3 to quit\n'
-                                '>> ')
+            choice = int(raw_input('Welcome to the stop watch!\n'
+                                   'Enter a command:\n'
+                                   '~~~~~~~~~~~~~~~\n'
+                                   '1 to START!\n'
+                                   '2 to STOP\n'
+                                   '3 to quit\n'
+                                   '>> '))
             if choice == 1:
                 STARTTIME = self.starttime()
+                print "Watch has started!"
                 self.ui()
             elif choice == 2:
                 stop = self.stoptime()
-                time_passed = self.time_bewtween(stop, STARTTIME)
+                print "Watch has stopped!"
+                time_passed = self.time_between(stop)
                 format_timepassed = self.format_time(time_passed)
                 print format_timepassed, "has passed."
                 raw_input("Press enter to continue...")
